dbf0c061002f6d35993d607b40ed28fa09e17aed
LinYenCheng <tmpss91015@gmail.com>
2023-01-07 14:12:59 +0800

    Add Math class 40

--- GitBranchApp/src/GitBranchApp1_40/MainApp_40.java
+++ GitBranchApp/src/GitBranchApp1_40/MainApp_40.java
@@ -3,8 +3,22 @@ package GitBranchApp1_40;
 public class MainApp_40 {
 
 	public static void main(String[] args) {
-		//leader 40
+		// leader 40
 		System.out.println("1st version 40");
+		// leader 40 new content
+		Math cMath=new Math();
+		System.out.println();
+		System.out.println(cMath.add(10, 20));
+
 	}
 
+}
+
+class Math {
+	Math(){
+		
+	}
+	int add(int a, int b) {
+		return a + b;
+	}
 }
